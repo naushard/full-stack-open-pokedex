f64a5cdb0a9750d707f9599de8b60af1a8f99bbe
naushard <naushard@singnet.com.sg>
2024-08-01 13:21:25 +0800

    ex 11.9 last exercise

--- e2e-tests/mytest.spec.js
+++ e2e-tests/mytest.spec.js
@@ -6,5 +6,22 @@ describe('Pokedex', () => {
     await page.goto('http://localhost:8080/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+
+  test('Can be navigated to Charmander page', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await expect(page.getByText('charmander')).toBeVisible()
+
+    await page.getByRole('link', { name: 'charmander' }).click()
+    
+    await expect(page.getByText('charmander')).toBeVisible()
+    await expect(page.getByText('next')).toBeVisible()
+    await expect(page.getByText('previous')).toBeVisible()
+    await expect(page.getByText('home')).toBeVisible()
+    
+
+
   })
+
+
 })
